75923ae57c476e9bf18703a6de36274992b92cc4
qubies <tobias@renwick.tech>
2017-10-05 14:22:34 -0600

    adds read

---
+++ uio/tests/DumpSum/readString.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <string.h>
+#include <openssl/sha.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <errno.h>
+
+int main(int argc, char ** argv){
+
+    long size;
+    char * file;
+    void * map_region;
+    char * text_array;
+    int i,fd;
+
+    if (argc!=3){
+        fprintf(stderr, "USAGE: sum <file> <size in MB>\n");
+        exit(-1);
+    }
+
+    size=atol(argv[2])*1024*1024;
+ 
+    if ((fd=open(argv[1], O_RDWR)) < 0){
+        fprintf(stderr, "ERROR: cannot open file\n");
+        exit(-1);
+    }
+
+    /* With UIO drivers, the offset selects the memory region beginning at 0 and counting by page offsets */
+    if ((map_region=mmap(NULL, size, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 1 * getpagesize())) == MAP_FAILED){
+        fprintf(stderr, "ERROR: cannot mmap file\n");
+    }
+
+    text_array = (char *) map_region;
+    printf("%s\n", text_array);
+
+    munmap(map_region,size);
+    close(fd);
+
+    printf("[SUM] Exiting...\n");
+}
