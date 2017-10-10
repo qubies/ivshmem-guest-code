a0542aa78023d5204e77438a79ab03a357342f00
qubies <tobias@renwick.tech>
2017-10-09 19:20:19 -0600

    revert

--- uio/tests/DumpSum/workingTogether/waitForFriends.c
+++ uio/tests/DumpSum/workingTogether/waitForFriends.c
@@ -14,29 +14,56 @@
 #include "Nahanni.h"
 #include "generalFunctions.h"
 
-sem_t *booger;
-
 typedef struct {
 	int val;
 	sem_t lock;
 } lockableInt;
 //adds 10 million onto the integer in increments of 1.
 void add10M(lockableInt *L, lockableInt *I) {
-	while (L->val != 1) { //cheezy sleep while it waits for the start flag to be set. 
-	} 
-	booger = sem_open("BOOGER1", O_EXCL);
 	for (int x = 0; x < 100000000; x++) {
-		if (sem_wait(booger) != 0)  {
+		while (L->val != 1) { //cheezy sleep while it waits for the start flag to be set. 
+		} 
+		if (sem_wait(&(I->lock)) != 0)  {
 			perror("Sem Wait Failed.\n");
 		}
 		(I->val)++;
 		if ((I->val) % 10000000 == 0) {
 			printf("My Number is:%d\n", I->val);
 		}
-		if (sem_post(booger) != 0) {
+		if (sem_post(&(I->lock)) != 0) {
 			perror("Sem Post Failed.\n");
 		}
 	}
+}
+
+void initialize(Nahanni *NN) {
+	
+	lockableInt *lockables = (lockableInt *)(NN->Memory);
+	lockableInt *I = &lockables[0]; 
+	lockableInt *L = &lockables[1]; 
+	if (sem_init(&(I->lock), 1, 1) != 0) {
+		errPrint("Problem Initializing Semaphore. Aborting.\n");
+		exit(EXIT_FAILURE);
+	}
+	I->val = 0; //initialize the counter too ... because its prettier. Its not really needed
+	L->val = 1; //initialise the waiter to stall
+	printf("The before Value:%d\n", I->val);
+	add10M(L,I);
+	printf("The after Value:%d\n", I->val);
+	freeNahanni(NN);
+
+}
+
+void wait(Nahanni *NN) {
+	lockableInt *lockables = (lockableInt *)(NN->Memory);
+	lockableInt *I = &lockables[0]; 
+	lockableInt *L = &lockables[1]; 
+	L->val = 0; //initialise the waiter to stall
+	I->val = 0; //initialize the counter too ... because its prettier. Its not really needed
+	printf("The before Value:%d\n", I->val);
+	add10M(L,I);
+	printf("The after Value:%d\n", I->val);
+	freeNahanni(NN);
 }
 
 int main (int argc, char*argv[]) {
@@ -45,30 +72,10 @@ int main (int argc, char*argv[]) {
 		exit(EXIT_FAILURE);
 	}
 	Nahanni *NN = NewNahanni(argv[1], atoi(argv[2])); //make the Nahanni.
-
-	lockableInt *lockables = (lockableInt *)(NN->Memory);
-	lockableInt *I = &lockables[0]; 
-	lockableInt *L = &lockables[1]; 
-	
-
 	if (atoi(argv[3]) == 1) {
-		booger = sem_open("BOOGER1", O_CREAT, 0644, 1);
-		I->val = 0; //initialize the counter too ... because its prettier. Its not really needed
-		L->val = 1; //initialise the waiter to stall
+		initialize(NN);
 	} else {
-		L->val = 0; //initialise the waiter to stall
-		I->val = 0; //initialize the counter too ... because its prettier. Its not really needed
+		wait(NN);
 	}
-	sem_unlink("BOOGER1");
-	
-	printf("The before Value:%d\n", I->val);
-	
-	add10M(L,I);
-	
-	printf("The after Value:%d\n", I->val);
-	
-	freeNahanni(NN);
-	sem_close(booger);
 }
-
 
