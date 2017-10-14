7e5a8d321aff1a51b5cfa3d574cf849879612b7e
qubies <tobias@renwick.tech>
2017-10-14 17:39:31 -0600

    try

--- uio/tests/DumpSum/workingTogether/waitForFriends.c
+++ uio/tests/DumpSum/workingTogether/waitForFriends.c
@@ -14,25 +14,25 @@
 #include "Nahanni.h"
 #include "generalFunctions.h"
 
-pthread_spinlock_t lock;
+pthread_spinlock_t *lock;
 //adds 10 million onto the integer in increments of 1.
 void add10M(int *L, int *I) {
 	while (*L != 1) { //cheezy sleep while it waits for the start flag to be set. 
 	} 
 	printf("starting...\n");
 	for (int x = 0; x < 100000000; x++) {
-		pthread_spin_lock(&lock);
+		pthread_spin_lock(lock);
 		(*I)++;
 		if ((*I) % 10000000 == 0) {
 			printf("My Number is:%d\n", *I);
 		}
-		pthread_spin_unlock(&lock); 
+		pthread_spin_unlock(lock); 
 	}
 }
 
 void initialize(Nahanni *NN, int *L, int *I) {
 	
-	if (pthread_spin_init(&lock, 1) != 0) {
+	if (pthread_spin_init(lock, 1) != 0) {
 		errPrint("Problem Initializing Semaphore. Aborting.\n");
 		exit(EXIT_FAILURE);
 	}
@@ -52,10 +52,8 @@ int main (int argc, char*argv[]) {
 		exit(EXIT_FAILURE);
 	}
 	Nahanni *NN = NewNahanni(argv[1], atoi(argv[2])); //make the Nahanni.
-
-	int *ints = (int *) &NN->Memory;
-	
-	lock = *((pthread_spinlock_t *) &(((int *)NN->Memory)[10]));
+	lock = (pthread_spinlock_t *) &NN->Memory;
+	int *ints = (int *) &(((pthread_spinlock_t *)(NN->Memory))[10]);
 
 	int *I = &ints[0]; 
 	int *L = &ints[1];
