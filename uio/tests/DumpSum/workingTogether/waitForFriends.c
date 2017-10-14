140d5491e45b15b2ad6be97e947cfd6f599fcb5d
qubies <tobias@renwick.tech>
2017-10-14 17:58:06 -0600

    wierd.

--- uio/tests/DumpSum/workingTogether/waitForFriends.c
+++ uio/tests/DumpSum/workingTogether/waitForFriends.c
@@ -14,36 +14,40 @@
 #include "Nahanni.h"
 #include "generalFunctions.h"
 
-pthread_spinlock_t *lock;
+typedef struct {
+	int val;
+	pthread_spinlock_t lock;
+}lockableInt;
+
 //adds 10 million onto the integer in increments of 1.
-void add10M(int *L, int *I) {
-	while (*L != 1) { //cheezy sleep while it waits for the start flag to be set. 
+void add10M(lockableInt **ints) {
+	while (ints[1]->val != 1) { //cheezy sleep while it waits for the start flag to be set. 
 	} 
 	printf("starting...\n");
 	for (int x = 0; x < 100000000; x++) {
-		pthread_spin_lock(lock);
-		(*I)++;
-		if ((*I) % 10000000 == 0) {
-			printf("My Number is:%d\n", *I);
+		pthread_spin_lock(&(ints[0]->lock));
+		(ints[0]->val)++;
+		if ((ints[0]->val) % 10000000 == 0) {
+			printf("My Number is:%d\n", ints[0]->val);
 		}
-		pthread_spin_unlock(lock); 
+		pthread_spin_unlock(&(ints[0]->lock));
 	}
 }
 
-void initialize(Nahanni *NN, int *L, int *I) {
+void initialize(lockableInt **ints) {
 	
-	if (pthread_spin_init(lock, 1) != 0) {
+	if (pthread_spin_init(&(ints[0]->lock), 1) != 0) {
 		errPrint("Problem Initializing Semaphore. Aborting.\n");
 		exit(EXIT_FAILURE);
 	}
-	*I = 0; //initialize the counter too ... because its prettier. Its not really needed
-	*L = 1; //initialise the waiter to start
+	ints[0]->val = 0;
+	ints[1]->val = 1;
 
 }
 
-void wait(Nahanni *NN, int *L, int *I) {
-	*L = 0; //initialise the waiter to stall
-	*I = 0; //initialize the counter too ... because its prettier. Its not really needed
+void wait(lockableInt **ints) {
+	ints[0]->val = 0; //initialise the waiter to stall
+	ints[1]->val = 0; //initialize the counter too ... because its prettier. Its not really needed
 }
 
 int main (int argc, char*argv[]) {
@@ -52,19 +56,17 @@ int main (int argc, char*argv[]) {
 		exit(EXIT_FAILURE);
 	}
 	Nahanni *NN = NewNahanni(argv[1], atoi(argv[2])); //make the Nahanni.
-	lock = (pthread_spinlock_t *) &NN->Memory;
-	int *ints = (int *) &(NN->Memory[50]);
+	
+	lockableInt **ints = (lockableInt **) NN->Memory;
 
-	int *I = &ints[0]; 
-	int *L = &ints[1];
 	if (atoi(argv[3]) == 1) {
-		initialize(NN, L, I);
+		initialize(ints);
 	} else {
-		wait(NN, L, I);
+		wait(ints);
 	}
-	printf("The before Value:%d\n", *I);
-	add10M(L,I);
-	printf("The after Value:%d\n", *I);
+	printf("The before Value:%d\n", ints[0]->val);
+	add10M(ints);
+	printf("The after Value:%d\n", ints[0]->val);
 	freeNahanni(NN);
 }
 
