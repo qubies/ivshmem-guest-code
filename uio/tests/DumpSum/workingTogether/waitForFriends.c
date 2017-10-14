9120cd6959d918a8274c178013f05ed26605a081
qubies <tobias@renwick.tech>
2017-10-14 15:20:06 -0600

    spinlock 1

--- uio/tests/DumpSum/workingTogether/waitForFriends.c
+++ uio/tests/DumpSum/workingTogether/waitForFriends.c
@@ -9,25 +9,25 @@
 #include <unistd.h>
 #include <errno.h>
 #include <time.h>
-#include <semaphore.h>
+#include <pthread.h>
 
 #include "Nahanni.h"
 #include "generalFunctions.h"
 
-sem_t *lock;
+pthread_spinlock_t *lock;
 //adds 10 million onto the integer in increments of 1.
 void add10M(int *L, int *I) {
 	while (*L != 1) { //cheezy sleep while it waits for the start flag to be set. 
 	} 
 	for (int x = 0; x < 100000000; x++) {
-		if (sem_wait(lock) != 0)  {
+		if (pthread_spin_lock(lock) != 0)  {
 			perror("Sem Wait Failed.\n");
 		}
 		(*I)++;
 		if ((*I) % 10000000 == 0) {
 			printf("My Number is:%d\n", *I);
 		}
-		if (sem_post(lock) != 0) {
+		if (pthread_spin_unlock(lock) != 0) {
 			perror("Sem Post Failed.\n");
 		}
 	}
@@ -35,7 +35,7 @@ void add10M(int *L, int *I) {
 
 void initialize(Nahanni *NN, int *L, int *I) {
 	
-	if (sem_init(lock, 1, 1) != 0) {
+	if (pthread_spin_init(lock, 1) != 0) {
 		errPrint("Problem Initializing Semaphore. Aborting.\n");
 		exit(EXIT_FAILURE);
 	}
@@ -55,8 +55,8 @@ int main (int argc, char*argv[]) {
 		exit(EXIT_FAILURE);
 	}
 	Nahanni *NN = NewNahanni(argv[1], atoi(argv[2])); //make the Nahanni.
-	lock = (sem_t *) &NN->Memory;
-	int *ints = (int *) &(((sem_t *)NN->Memory)[1]);
+	lock = (pthread_spinlock_t *) &NN->Memory;
+	int *ints = (int *) &(((pthread_spinlock_t *)NN->Memory)[1]);
 
 	int *I = &ints[0]; 
 	int *L = &ints[1];
